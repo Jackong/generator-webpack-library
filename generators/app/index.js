2b79ca3c2b8775207c540109b897c4f989766553
Jackong <JackongC@gmail.com>
2015-09-15 17:08:34 +0800

    init project

---
+++ generators/app/index.js
@@ -0,0 +1,59 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the sweet ' + chalk.red('WebpackLibrary') + ' generator!'
+    ));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        {
+            appname: this.appname,
+            name: this.user.git.name(),
+            email: this.user.git.email()
+        }
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+    },
+    gulp: function() {
+        this.fs.copy(
+            this.templatePath('gulpfile.js'),
+            this.destinationPath('gulpfile.js')
+        )
+    },
+    webpack: function() {
+        this.fs.copyTpl(
+            this.templatePath('webpack.config.js'),
+            this.destinationPath('webpack.config.js'),
+            {
+                appname: this.appname
+            }
+        )
+    },
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
